3f6bac0bc3c17383bee6abf725221800a30dba08
Administrator <rann86@mail.ru>
2022-05-28 13:27:22 +0300

    'error_500'

--- simpleapp/urls.py
+++ simpleapp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import ProductsList, ProductDetail # импортируем наше представление
+from .views import Products  # импортируем наше представление
  
  
 urlpatterns = [
     # path — означает путь. В данном случае путь ко всем товарам у нас останется пустым, позже станет ясно почему
-    path('', ProductsList.as_view()),
-    path('<int:pk>', ProductDetail.as_view()),
+    path('', Products.as_view()),
 ]
